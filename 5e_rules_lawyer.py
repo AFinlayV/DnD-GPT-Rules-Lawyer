8977dfee223dfeeebb71822e4ffb7abd3b520fdd
AFinlayV <afinlayv@gmail.com>
2023-02-08 22:39:09 -0500

    Version 0.2 got it talking to discord

--- 5e_rules_lawyer.py
+++ 5e_rules_lawyer.py
@@ -16,6 +16,7 @@ import gpt_index
 import datetime
 from numpy.linalg import norm
 import numpy as np
+import discord
 
 with open("/Users/alexthe5th/Documents/API Keys/OpenAI_API_key.txt", "r") as f:
     openai.api_key = f.read().strip()
@@ -41,6 +42,14 @@ def save_json(filepath, payload):
         json.dump(payload, outfile, ensure_ascii=False, sort_keys=True, indent=2)
 
 
+SRD = load_json('docs/srd.json')
+intents = discord.Intents.all()
+client = discord.Client(intents=intents,
+                        shard_id=0,
+                        shard_count=1,
+                        reconnect=True)
+
+
 def timestamp_to_datetime(unix_time):
     return datetime.datetime.fromtimestamp(unix_time).strftime("%A, %B %d, %Y at %I:%M%p %Z")
 
@@ -57,7 +66,7 @@ def similarity(v1, v2):
     return np.dot(v1, v2) / (norm(v1) * norm(v2))  # return cosine similarity
 
 
-def fetch_rule(srd, query, count=2):
+def fetch_rule(srd, query, count=3):
     qvector = gpt3_embedding(query)
     scores = {}
     # use similarity() to compare the qvector to the vector of each rule, then return the top 5
@@ -67,7 +76,7 @@ def fetch_rule(srd, query, count=2):
     return top
 
 
-def gpt3_completion(prompt, engine='text-davinci-003', temp=0.0, tokens=512, top_p=1.0, freq_pen=0.0, pres_pen=0.0):
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.0, tokens=256, top_p=1.0, freq_pen=0.0, pres_pen=0.0):
     max_retry = 5
     retry = 0
     prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
@@ -93,6 +102,91 @@ def gpt3_completion(prompt, engine='text-davinci-003', temp=0.0, tokens=512, top
             sleep(1)
 
 
+def process_message(discord_message):
+    discord_text = discord_message.content
+    user = discord_message.author.name
+    try:
+        query = discord_message.content
+        top = fetch_rule(SRD, query)
+        # make the list a string
+        rules = ' '.join(top)
+        # for rule in top:
+        #     print(rule, "\n\n====================\n\n")
+        prompt = f"""
+                I am a dungeon master named "RulesLawyer" and I have been asked a question about the rules of Dungeons and Dragons.
+                I will only answer questions based on the rules given below, and I will not say anything that can't be inferred from them.
+                I will also refuse to answer questions that are not about the rules of Dungeons and Dragons.
+                Using the only the information below (from the SRD), I will answer the question as best I can.
+
+                +++++++
+
+                Rules: {rules}
+
+                +++++++
+
+                Question: {query}
+
+                Below I will write an answer to the question, as it applies to Dungeons and Dragons 5e. I will only use infrormation from the rules above.
+                and I will provide a long, detailed, and thorough answer. I will also provide the text of the rule that my answer is based on.
+                RulesLawyer:"""
+        response = gpt3_completion(prompt)
+        return response
+    except Exception as oops:
+        return {'output': 'Error in process_message: %s' % oops, 'user': user}
+
+
+@client.event
+async def on_message(discord_message, timeout=60):
+    discord_text = discord_message.content
+    user = discord_message.author.name
+    try:
+        if not discord_message.author.bot:
+                await discord_message.channel.send(f'Generating response for {user}:')
+                await asyncio.wait_for(
+                    send_response(discord_message),
+                    timeout=timeout)
+    except asyncio.TimeoutError:
+        print(f'Response timed out. \n {user}: {discord_text[:20]}...')
+    except Exception as oops:
+        await discord_message.channel.send(f'Error: {oops} \n {user}: {discord_text[:20]}...')
+
+
+async def send_response(discord_message):
+    try:
+        channel = discord_message.channel
+        user = discord_message.author.name
+        discord_text = discord_message.content
+        response = process_message(discord_message)
+        # Create a new task for sending the response
+        await discord_message.channel.send(f'Rules Lawyer Says:\n {response}')
+    except Exception as oops:
+        # Something else went wrong while sending the response
+        await discord_message.channel.send('Error sending response:', oops)
+
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(CHAN_ID)
+    if channel is not None:
+        await channel.send(f"{client.user} has connected to Discord!")
+    else:
+        print("Channel not found")
+
+
+if __name__ == "__main__":
+    with open("/Users/alexthe5th/Documents/API Keys/OpenAI_API_key.txt", "r") as f:
+        key = f.read().strip()
+        openai.api_key = key
+    auth = load_json('/Users/alexthe5th/Documents/API Keys/RulesLawyerAuth.txt')
+    TOKEN = auth['token'].strip()
+    CHAN_ID = int(auth['chan_id'].strip())
+    try:
+        client.run(TOKEN)
+    except Exception as oops:
+        print('Error in main:', oops)
+
+'''
+Old code for testing in command line:
 def main():
     srd = load_json('docs/srd.json')
     while True:
@@ -106,14 +200,23 @@ def main():
         I am a dungeon master named "RulesLawyer" and I have been asked a question about the rules of Dungeons and Dragons.
         I will only answer questions based on the rules given below, and I will not say anything that can't be inferred from them.
         I will also refuse to answer questions that are not about the rules of Dungeons and Dragons.
-        Using the Information below (from the SRD), I will answer the question as best I can.
+        Using the only the information below (from the SRD), I will answer the question as best I can.
+        
+        +++++++
+        
         Rules: {rules}
+        
+        +++++++
+        
         Question: {query}
-        RulesLawyer's Answer, including the text of the rule the answer is based on:
-        """
+        
+        Below I will write an answer to the question, as it applies to Dungeons and Dragons 5e. I will only use infrormation from the rules above.
+        and I will provide a long, detailed, and thorough answer. I will also provide the text of the rule that my answer is based on.
+        RulesLawyer:"""
         response = gpt3_completion(query)
         print(response)
 
 
 if __name__ == "__main__":
     main()
+'''
